d34e3bf7b8aacdc167f1b18bf065d3e7b8afbd7a
Barry Peddycord III <ishara@isharacomix.org>
2016-03-29 15:31:25 -0400

    Working on puppet demo

--- production/modules/ifupdown/manifests/init.pp
+++ production/modules/ifupdown/manifests/init.pp
@@ -1,13 +1,18 @@
 class ifupdown {
-    file { '/etc/network/interfaces':
+
+    $interfaces_hiera = hiera('interfaces')
+    $intvars = $interfaces_hiera[$hostname]
+    $networks = hiera('networks')
+
+    file { '/etc/network/interfaces_test':
         owner   => root,
         group   => root,
         mode    => '0644',
         content => template('ifupdown/interfaces.erb'),
     }
 
-    exec { '/sbin/ifdown -a && /sbin/ifup -a':
-        subscribe   => File['/etc/network/interfaces'],
-        refreshonly => true
-    }
+    #exec { '/sbin/ifdown -a && /sbin/ifup -a':
+    #    subscribe   => File['/etc/network/interfaces'],
+    #    refreshonly => true
+    #}
 }
--- production/modules/ifupdown2/manifests/init.pp
+++ production/modules/ifupdown2/manifests/init.pp
@@ -1,13 +1,13 @@
 class ifupdown2 {
-    file { '/etc/network/interfaces':
+    file { '/etc/network/interfaces_test':
         owner   => root,
         group   => root,
         mode    => '0644',
         content => template('ifupdown2/interfaces.erb'),
     }
 
-    exec { '/sbin/ifreload -a':
-        subscribe   => File['/etc/network/interfaces'],
-        refreshonly => true
-    }
+    #exec { '/sbin/ifreload -a':
+    #    subscribe   => File['/etc/network/interfaces'],
+    #    refreshonly => true
+    #}
 }
