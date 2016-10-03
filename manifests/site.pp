9d2fdaac083343205476aa682733591d944aa0d2
Barry Peddycord III <ishara@isharacomix.org>
2016-10-03 10:05:28 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,4 @@
 
-# Set default provider on CL 3+ as systemd
-if $::os['name'] == 'CumulusLinux' and $::os['release']['major']>='3' {
-  Service {
-    provider => 'systemd',
-  }
-}
-
-
 # Any node that doesn't fit into another category will use the default node
 # instead.
 node default {
