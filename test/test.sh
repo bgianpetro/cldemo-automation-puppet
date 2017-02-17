de1175cdd14d98c44f628a70b8cb46f0b2ba9619
Barry Peddycord III <ishara@isharacomix.org>
2017-02-17 10:19:23 -0500

    Create test.sh

---
+++ test/test.sh
@@ -0,0 +1,10 @@
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+sudo dpkg -i puppetlabs-release-pc1-xenial.deb
+sudo apt-get update
+sudo apt-get install puppetserver -qy
+sudo rm -rf /etc/puppetlabs/code/environments/production
+sudo ln -s  /home/cumulus/cldemo-automation-puppet/ /etc/puppetlabs/code/environments/production
+sudo service puppetserver restart
+python install-puppet-agents.py leaf01,leaf02,spine01,spine02,server01,server02
+ssh server01 wget -T 30 172.16.2.101
+ssh server01 cat index.html
